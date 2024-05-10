e4632d225ec9032c8b65fadddbb316c215169de9
Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2024-05-10 22:48:02 +0200

    MyStackProjectTests added

---
+++ MyStackProject/MyStackSolution/MyStackLib/MyStack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace MyStackLib
+{
+    public class MyStack
+    {
+        public MyStack()
+        {
+        }
+
+        private int _elementCount;
+        private List<int> _items = new List<int>();
+
+        public void Push(int item)
+        {
+            _elementCount = _elementCount - 1;
+            _items.Add(item);
+        }
+
+
+        public bool IsEmpty()
+        {
+            if (_elementCount > 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+        public int Pull()
+        {
+            if (_elementCount == 0)
+            {
+                return int.MinValue;
+            }
+
+            var tmpValue = _items[_elementCount - 1];
+            _items.RemoveAt(_elementCount - 1);
+            _elementCount--;
+            return tmpValue;
+        }
+    }
+}
---
+++ MyStackProject/MyStackSolution/MyStackProjectTests/MyStackTests.cs
@@ -0,0 +1,55 @@
+﻿using MyStackLib;
+using NUnit.Framework;
+
+namespace MyStackProjectTests
+{
+    [TestFixture]
+    public class MyStackTests
+    {
+        [Test]
+        public void AddOneIntegerInStack_Test()
+        {
+            var mystack = new MyStack();
+            mystack.Push(5);
+        }
+
+        [Test]
+        public void IsMyStackEmpty_Test()
+        {
+            var mystack = new MyStack();
+            var isEmpty = mystack.IsEmpty();
+            Assert.AreEqual(isEmpty, true);
+        }
+
+        [Test]
+        public void PushOneNumber_IsMyStackEmpty_Test()
+        {
+            var mystack = new MyStack();
+            mystack.Push(5);
+            var isEmpty = mystack.IsEmpty();
+            Assert.AreEqual(isEmpty, false);
+        }
+
+        [Test]
+        public void PushTwoNumbers_PullOne_Test()
+        {
+            var myStack = new MyStack();
+            myStack.Push(5);
+            myStack.Push(50);
+            int number = myStack.Pull();
+            Assert.AreEqual(number, 50);
+        }
+
+        [Test]
+        public void PushOne_PullTwice_Test()
+        {
+            var myStack = new MyStack();
+            myStack.Push(5);
+            myStack.Pull();
+            var number = myStack.Pull();
+
+
+            Assert.AreEqual(number, int.MinValue);
+        }
+    }
+}
